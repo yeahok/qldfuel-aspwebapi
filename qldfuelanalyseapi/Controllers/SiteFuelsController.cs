9b85808f5100619105199deb9d52489f76a82339
yeahok <yeahok@users.noreply.github.com>
2020-07-11 22:17:08 +1000

    Remove create/update/delete functions from controllers

--- qldfuelanalyseapi/Controllers/SiteFuelsController.cs
+++ qldfuelanalyseapi/Controllers/SiteFuelsController.cs
@@ -41,66 +41,6 @@ namespace qldfuelanalyseapi.Controllers
             return siteFuel;
         }
 
-        // PUT: api/SiteFuels/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutSiteFuel(int id, SiteFuel siteFuel)
-        {
-            if (id != siteFuel.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(siteFuel).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!SiteFuelExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/SiteFuels
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPost]
-        public async Task<ActionResult<SiteFuel>> PostSiteFuel(SiteFuel siteFuel)
-        {
-            _context.SiteFuel.Add(siteFuel);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetSiteFuel", new { id = siteFuel.Id }, siteFuel);
-        }
-
-        // DELETE: api/SiteFuels/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<SiteFuel>> DeleteSiteFuel(int id)
-        {
-            var siteFuel = await _context.SiteFuel.FindAsync(id);
-            if (siteFuel == null)
-            {
-                return NotFound();
-            }
-
-            _context.SiteFuel.Remove(siteFuel);
-            await _context.SaveChangesAsync();
-
-            return siteFuel;
-        }
-
         private bool SiteFuelExists(int id)
         {
             return _context.SiteFuel.Any(e => e.Id == id);
